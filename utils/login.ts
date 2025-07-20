22f3f2f03a5701f9d2892826b3735f195769fd9c
Alex Bazin <alexis.bazin@sky.uk>
2025-07-20 13:08:24 +0100

    final few updates

--- utils/login.ts
+++ utils/login.ts
@@ -1,12 +1,15 @@
 import { Page } from "@playwright/test";
 
+const email: string = "FILL IN YOUR EMAIL HERE";
+const password: string = "FILL IN YOUR PASSWORD HERE";
+
 export const login = async (page: Page) => {
   await page.goto("http://3.8.242.61/");
   await page.getByRole("button", { name: "Login" }).click();
   await page
     .getByRole("textbox", { name: "Email" })
-    .fill("alexjbazin@gmail.com");
-  await page.getByRole("textbox", { name: "Password" }).fill("Bilingual123!");
+    .fill(email);
+  await page.getByRole("textbox", { name: "Password" }).fill(password);
   await page.getByRole("button", { name: "Log in" }).click();
   await page.waitForLoadState("networkidle");
 }
